bdd49abc5e066eab01998a9769997dd1d6149d92
cjshinning <784319417@qq.com>
2021-08-03 17:26:13 +0800

    更多路由配置

--- index.js
+++ index.js
@@ -4,5 +4,11 @@ const app = express();
 app.get('/', (req, res) => {
     res.send('Hello world!');
 })
+app.get('/customer', (req, res) => {
+    res.send('customer page!');
+})
+app.get('/admin', (req, res) => {
+    res.send('admin page!');
+})
 
 app.listen(3000);
