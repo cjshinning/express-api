a88f5db7046d35586fd806a7bc7e5fb2e760b19e
cjshinning <784319417@qq.com>
2021-08-04 10:08:08 +0800

    路由参数1

--- index.js
+++ index.js
@@ -1,18 +1,24 @@
 const express = require('express');
 const app = express();
 
-app.use('/home', (request, response, next) => {
+app.use('*', (request, response, next) => {
     response.writeHead(200, {'Content-Type': 'text/plain'});
+    next();
+});
+
+app.get('/', (request, response) => {
     response.end('Welcome to the homepage!\n');
 });
 
-app.use('/about', (request, response, next) => {
-    response.writeHead(200, {'Content-Type': 'text/plain'});
+app.get('/about', (request, response) => {
     response.end('Welcome to the about page!\n');
 });
 
-app.use((request, response) => {
-    response.writeHead(404, {'Content-Type': 'text/plain'});
+app.get('/hello/:who', (request, response) => {
+    response.end('Hello, ' + request.params.who + '.');
+});
+
+app.get('*', (request, response) => {
     response.end('404 error!\n');
 })
 
