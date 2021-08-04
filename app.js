23a9ee1a6ca6dbd61cee7af2a5413c82c90a3e6d
cjshinning <784319417@qq.com>
2021-08-04 17:10:44 +0800

    动态模板

--- app.js
+++ app.js
@@ -1,16 +1,36 @@
 const express = require('express');
 const app = express();
+const bodyParser = require('body-parser');
+
+const hbs = require('hbs');
+
+const blogEngine = require('./blog');
+
+app.set('view engine', 'html');
+
+app.engine('html', hbs.__express);
+app.use(bodyParser.json());
 
 app.get('/', (req, res) => {
-    res.sendFile(__dirname + '/views/index.html');
+    res.render('index', {
+        title: '最近文章',
+        entries: blogEngine.getBlogEntries()
+    });
 })
 
 app.get('/about', (req, res) => {
-    res.sendFile(__dirname + '/views/about.html');
+    res.render('about', {
+        title: '自我介绍'
+    });
 })
 
-app.get('/article', (req, res) => {
-    res.sendFile(__dirname + '/views/article.html');
+app.get('/article/:id', (req, res) => {
+    const entry = blogEngine.getBlogEntry(req.params.id);
+    console.log(entry);
+    res.render('article', {
+        title: entry.title,
+        blog: entry
+    });
 })
 
 app.listen(3000);
