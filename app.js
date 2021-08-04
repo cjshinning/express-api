e5962a2b0da7e1867f0130f3bdffe6fcb70289d9
cjshinning <784319417@qq.com>
2021-08-04 15:47:21 +0800

    静态网页模板

--- app.js
+++ app.js
@@ -1,34 +1,16 @@
-const path = require('path');
 const express = require('express');
 const app = express();
 
-app.set('port', process.env.PORT || 3000);
-
-app.set('views', path.join(__dirname, 'views'));
-
-app.set('View engine', 'jade');
-
-// app.use(express.favicon());
-// app.use(express.logger('dev'));
-// app.use(express.bodyParser());
-// app.use(express.methodOverride());
-// app.use(app.router);
-
 app.get('/', (req, res) => {
-    const body = 'Hello World';
-    res.setHeader('Content-Type', 'text/plain');
-    res.setHeader('Content-Length', body.length);
-    res.end(body);
+    res.sendFile(__dirname + '/views/index.html');
 })
 
-// app.get('/api', (req, res) => {
-//     res.send({name: '张三', age: 40});
-// })
+app.get('/about', (req, res) => {
+    res.sendFile(__dirname + '/views/about.html');
+})
 
+app.get('/article', (req, res) => {
+    res.sendFile(__dirname + '/views/article.html');
+})
 
-const api = require('./routes/api');
-app.get('/api', api.index);
-
-app.use(express.static(path.join(__dirname, 'public')));
-
-app.listen(app.get('port'));
+app.listen(3000);
