8dd5b7b3eb5b67ef8ddcc42bf3b7187d868287ce
cjshinning <784319417@qq.com>
2021-08-04 09:31:13 +0800

    中间件的使用1

--- index.js
+++ index.js
@@ -1,6 +1,14 @@
 const express = require('express');
 const app = express();
 
-const routes = require('./routes')(app);
+app.use((request, response, next) => {
+    console.log(`In comes a ${request.method} to ${request.url}`);
+    next();
+});
+
+app.use((request, response) => {
+    response.writeHead(200, {'Content-Type': 'text/plain'});
+    response.end('Hello world!\n');
+})
 
 app.listen(3000);
